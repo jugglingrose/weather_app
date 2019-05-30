e0ec384d45a98bcc3331000450bf3945054e5859
Corean Gonzales <coreangonzales@gmail.com>
2019-05-30 13:22:47 -0500

    Added Hourly Component

---
+++ src/components/Hourly.js
@@ -0,0 +1,12 @@
+import React from 'react';
+/* http://api.openweathermap.org/data/2.5/forecast?q=london,uk&units=imperial&APPID=27aae5ebfd2aaddddcff5171637b34f3 */ 
+
+class Hourly extends React.Component {
+  render() {
+    return(
+      <div>hourly</div>
+    )
+  }
+}
+
+export default Hourly;
